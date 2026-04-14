b1c8c2e3fb8acaf6d64b012d7575cb9c514f4cd5
Travis Thompson <travis.thompson@quantinuum.com>
2026-04-14 12:18:37 +0200

    wip: update pebble hole graph

--- pytket/extensions/aqt/multi_zone_architecture/trap_architecture/pebble_hole_graph.py
+++ pytket/extensions/aqt/multi_zone_architecture/trap_architecture/pebble_hole_graph.py
@@ -1,25 +1,53 @@
-from networkx import Graph
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import TypeAlias
+
+from networkx import (
+    Graph,
+)
 
 from ..circuit.helpers import TrapConfiguration
 from .architecture import MultiZoneArchitectureSpec, PortId
+
+Node: TypeAlias = tuple[int, int]
+
+_EMPTY = -1
+_MAX_PATH_CANDIDATES = 8
+_MAX_MULTI_EVICTION_ZONE_CANDIDATES = 6
+_MAX_MULTI_TARGET_NODE_CANDIDATES = 4
+_NO_PATH = 10**9
+
+
+class PebbleMotionError(Exception):
+    pass
+
+
+@dataclass(frozen=True)
+class PebbleMove:
+    qubit: int
+    source: Node
+    target: Node
 
 
 class PebbleHoleGraph:
     def __init__(
         self, spec: MultiZoneArchitectureSpec, start_config: TrapConfiguration
     ):
-        self.pebble_graph: Graph[tuple[int, int]] = Graph()
-        self.zone_size: list[int] = []
+        self.pebble_graph: Graph = Graph()
+        self._zone_capacities: list[int] = []
+        self._zone_nodes: list[list[Node]] = []
         placement = start_config.zone_placement
         for zone_id, zone in enumerate(spec.zones):
             transport_max_cap = zone.max_ions_transport_op
+            self._zone_capacities.append(transport_max_cap)
+            zone_nodes: list[Node] = []
             occupancy = len(placement[zone_id])
-            self.zone_size.append(occupancy)
-            # add nodes
             for i in range(transport_max_cap):
-                occ = placement[zone_id][i] if i < occupancy else -1
-                self.pebble_graph.add_node((zone_id, i), occupant=occ)
-            # add inner zone edges
+                occ = placement[zone_id][i] if i < occupancy else _EMPTY
+                node = (zone_id, i)
+                self.pebble_graph.add_node(node, occupant=occ)
+                zone_nodes.append(node)
+            self._zone_nodes.append(zone_nodes)
             for i in range(transport_max_cap - 1):
                 self.pebble_graph.add_edge((zone_id, i), (zone_id, i + 1))
 
@@ -27,11 +55,45 @@ class PebbleHoleGraph:
         for connection in spec.connections:
             zone0 = connection.zone_port_spec0.zone_id
             port0 = connection.zone_port_spec0.port_id
-            node0 = (zone0, 0 if port0 == PortId.p0 else self.zone_size[zone0])
+            node0 = (zone0, 0 if port0 == PortId.p0 else self._zone_capacities[zone0] - 1)
             zone1 = connection.zone_port_spec1.zone_id
             port1 = connection.zone_port_spec1.port_id
-            node1 = (zone1, 0 if port1 == PortId.p0 else self.zone_size[zone1])
-            # TODO: update arch spec to include connection shuttle cost and use that as weight
+            node1 = (zone1, 0 if port1 == PortId.p0 else self._zone_capacities[zone1] - 1)
             self.pebble_graph.add_edge(
                 node0, node1, transport_cost=1, is_shuttle_edge=True
             )
+
+    def copy(self) -> PebbleHoleGraph:
+        copied = object.__new__(PebbleHoleGraph)
+        copied.pebble_graph = self.pebble_graph.copy(as_view=False)
+        copied._zone_capacities = self._zone_capacities.copy()
+        copied._zone_nodes = [zone_nodes.copy() for zone_nodes in self._zone_nodes]
+        return copied
+
+    def occupant(self, node: Node) -> int:
+        return int(self.pebble_graph.nodes[node]["occupant"])
+
+    def is_empty(self, node: Node) -> bool:
+        return self.occupant(node) == _EMPTY
+
+    def qubit_node(self, qubit: int) -> Node:
+        for node in self.pebble_graph.nodes:
+            if self.occupant(node) == qubit:
+                return node
+        raise PebbleMotionError(f"Qubit {qubit} is not placed on the pebble graph")
+
+    def empty_nodes(self) -> list[Node]:
+        return [node for node in self.pebble_graph.nodes if self.is_empty(node)]
+
+    def zone_nodes(self, zone: int) -> list[Node]:
+        return self._zone_nodes[zone].copy()
+
+    def qubits_in_zone(self, zone: int) -> list[int]:
+        return [
+            occupant
+            for node in self._zone_nodes[zone]
+            if (occupant := self.occupant(node)) != _EMPTY
+        ]
+
+    def placement(self) -> list[list[int]]:
+        return [self.qubits_in_zone(zone) for zone in range(len(self._zone_nodes))]
